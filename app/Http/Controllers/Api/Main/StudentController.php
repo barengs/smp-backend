6ca2200a77923cdaa3138c74a2b2f46af2fc8f4d
rofiareiv <kambing.rofi@gmail.com>
2025-04-26 15:05:57 +0700

    new branch

--- app/Http/Controllers/Api/Main/StudentController.php
+++ app/Http/Controllers/Api/Main/StudentController.php
@@ -12,7 +12,21 @@ class StudentController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            // Fetch all students from the database
+            $students = User::whereHas('student')->with(['student', 'roles'])->get();
+            if ($students->isEmpty()) {
+                return response()->json('data tidak ada', 404);
+            }
+            return response()->json($students, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to fetch students: ' . $e->getMessage(),
+            ], 500);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 
     /**
