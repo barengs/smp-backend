bcf99de0d0e30cc37c7a448efa59cc35f38efa29
rofiareiv <kambing.rofi@gmail.com>
2025-07-29 15:36:01 +0700

    perbaikan pada controller academik

--- app/Http/Controllers/Api/Master/AcademicYearController.php
+++ app/Http/Controllers/Api/Master/AcademicYearController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Master;
 
+use App\Models\AcademicYear;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AcademicYearResource;
