6e9e6934ff0fec74dfe0a580237e48cac192bad4
rofiareiv <kambing.rofi@gmail.com>
2025-08-10 07:13:49 +0700

    add control panel seeder

--- database/seeders/ControlPanelSeeder.php
+++ database/seeders/ControlPanelSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ControlPanel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class ControlPanelSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        ControlPanel::create([
+            'app_name' => 'Sistem Manajemen Pesantren',
+            'app_version' => '1.31.592',
+            'app_description' => 'Aplikasi manajemen pesantren yang terintegrasi.',
+            'app_logo' => 'logo.png',
+            'app_favicon' => 'favicon.ico',
+            'app_url' => 'https://smp.barengsaya.com',
+            'app_email' => 'smp.barengsaya.com',
+            'app_phone' => '1234567890',
+            'app_address' => 'Jl. Contoh, Kota Contoh, Negara Contoh',
+        ]);
     }
 }
