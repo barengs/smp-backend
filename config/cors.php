acf8cdb84de88da6b5d09733a99865eb45627296
rofiareiv <kambing.rofi@gmail.com>
2025-08-11 19:51:16 +0700

    allow domain on origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'https://smp.barengsaya.com'],
+    'allowed_origins' => ['*', 'https://smp.barengsaya.com', 'smp.barengsaya.com'],
 
     'allowed_origins_patterns' => [],
 
