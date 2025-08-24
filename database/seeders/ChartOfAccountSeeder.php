a381fd8455cfb4a43464f9f334bddeacff788831
rofiareiv <kambing.rofi@gmail.com>
2025-08-24 14:49:56 +0700

    update seeder coa

--- database/seeders/ChartOfAccountSeeder.php
+++ database/seeders/ChartOfAccountSeeder.php
@@ -16,7 +16,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '100000',
                 'account_name' => 'ASET',
-                'account_type' => 'Asset',
+                'account_type' => 'ASSET',
                 'level' => 'header',
                 'is_postable' => 'False',
                 'is_active' => 'True'
@@ -24,7 +24,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '200000',
                 'account_name' => 'KEWAJIBAN',
-                'account_type' => 'Liability',
+                'account_type' => 'LIABILITY',
                 'level' => 'header',
                 'is_postable' => 'False',
                 'is_active' => 'True'
@@ -32,7 +32,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '300000',
                 'account_name' => 'EKUITAS',
-                'account_type' => 'Equity',
+                'account_type' => 'EQUITY',
                 'level' => 'header',
                 'is_postable' => 'False',
                 'is_active' => 'True'
@@ -40,7 +40,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '400000',
                 'account_name' => 'PENDAPATAN',
-                'account_type' => 'Revenue',
+                'account_type' => 'REVENUE',
                 'level' => 'header',
                 'is_postable' => 'False',
                 'is_active' => 'True'
@@ -48,7 +48,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '420000',
                 'account_name' => 'Pendapatan Operasional Selain Bunga',
-                'account_type' => 'Revenue',
+                'account_type' => 'REVENUE',
                 'level' => 'subheader',
                 'parent_coa_code' => '400000',
                 'is_postable' => 'False',
@@ -57,7 +57,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '421000',
                 'account_name' => 'Pendapatan Jasa dan Administrasi',
-                'account_type' => 'Revenue',
+                'account_type' => 'REVENUE',
                 'level' => 'subheader',
                 'parent_coa_code' => '420000',
                 'is_postable' => 'False',
@@ -66,7 +66,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '421100',
                 'account_name' => 'Pendapatan Terkait Rekening',
-                'account_type' => 'Revenue',
+                'account_type' => 'REVENUE',
                 'level' => 'subheader',
                 'parent_coa_code' => '421000',
                 'is_postable' => 'False',
@@ -75,7 +75,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '421101',
                 'account_name' => 'Pendapatan Biaya Pendaftaran',
-                'account_type' => 'Revenue',
+                'account_type' => 'REVENUE',
                 'level' => 'detail',
                 'parent_coa_code' => '421100',
                 'is_postable' => 'True',
@@ -84,7 +84,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '500000',
                 'account_name' => 'BEBAN',
-                'account_type' => 'Expense',
+                'account_type' => 'EXPENSE',
                 'level' => 'header',
                 'is_postable' => 'False',
                 'is_active' => 'True'
@@ -92,7 +92,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '110000',
                 'account_name' => 'Kas dan Setara Kas',
-                'account_type' => 'Assets',
+                'account_type' => 'ASSET',
                 'level' => 'subheader',
                 'parent_coa_code' => '100000',
                 'is_postable' => 'False',
@@ -101,7 +101,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '111000',
                 'account_name' => 'Kas',
-                'account_type' => 'Assets',
+                'account_type' => 'ASSET',
                 'level' => 'subheader',
                 'parent_coa_code' => '110000',
                 'is_postable' => 'False',
@@ -110,7 +110,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '111100',
                 'account_name' => 'Kas di Khazanah',
-                'account_type' => 'Assets',
+                'account_type' => 'ASSET',
                 'level' => 'subheader',
                 'parent_coa_code' => '111000',
                 'is_postable' => 'False',
@@ -119,7 +119,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '111101',
                 'account_name' => 'Kas di Teller',
-                'account_type' => 'Assets',
+                'account_type' => 'ASSET',
                 'level' => 'detail',
                 'parent_coa_code' => '111100',
                 'is_postable' => 'True',
@@ -128,7 +128,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '111102',
                 'account_name' => 'Kas di ATM',
-                'account_type' => 'Assets',
+                'account_type' => 'ASSET',
                 'level' => 'detail',
                 'parent_coa_code' => '111100',
                 'is_postable' => 'True',
@@ -137,7 +137,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '210000',
                 'account_name' => 'Simpanan Nasabah',
-                'account_type' => 'Liability',
+                'account_type' => 'LIABILITY',
                 'level' => 'subheader',
                 'parent_coa_code' => '200000',
                 'is_postable' => 'False',
@@ -146,7 +146,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '211000',
                 'account_name' => 'Simpanan Bentuk Tabungan',
-                'account_type' => 'Liability',
+                'account_type' => 'LIABILITY',
                 'level' => 'subheader',
                 'parent_coa_code' => '210000',
                 'is_postable' => 'False',
@@ -155,7 +155,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '211100',
                 'account_name' => 'Tabungan Rupiah',
-                'account_type' => 'Liability',
+                'account_type' => 'LIABILITY',
                 'level' => 'subheader',
                 'parent_coa_code' => '211000',
                 'is_postable' => 'False',
@@ -164,7 +164,7 @@ class ChartOfAccountSeeder extends Seeder
             [
                 'coa_code' => '211101',
                 'account_name' => 'Simpanan Tabungan',
-                'account_type' => 'Liability',
+                'account_type' => 'LIABILITY',
                 'level' => 'detail',
                 'parent_coa_code' => '211100',
                 'is_postable' => 'True',
